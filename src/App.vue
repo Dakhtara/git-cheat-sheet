213db423e4a30592560b636f4afbf311c4d8e721
Anthony Matignon <anthony.matignon@codes-rousseau.fr>
2020-09-21 10:39:57 +0200

    Ajout des fichier json de config

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Git Cheat Sheet</h1>
+
+    <GitCommandsList title="Git Basic" :commands="gitBasic" />
+
+    <GitCommandsList title="Annulation des moficiation" :commands="gitUndoingChanges" />
+    <GitCommandsList title="Réecrire l'historique" :commands="gitRewritingHistory" />
+    <GitCommandsList title="Git Branches" :commands="gitBranches" />
+    <GitCommandsList title="Dépôts Distants" :commands="gitRemoteRepositories" />
+
+    <br />
+    <h1>Options additionnels</h1>
+
+    <GitCommandsList title="Git Config" :commands="gitConfig" />
+    <GitCommandsList title="Git Log" :commands="gitLog" />
+    <GitCommandsList title="Git Diff" :commands="gitDiff" />
+    <GitCommandsList title="Git Reset" :commands="gitReset" />
+    <GitCommandsList title="Git Rebase" :commands="gitRebase" />
+    <GitCommandsList title="Git Pull" :commands="gitPull" />
+    <GitCommandsList title="Git Push" :commands="gitPush" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import gitBasic from "./git-basic.json";
+import gitUndoingChanges from "./git-undoing-changes.json";
+import gitRewritingHistory from "./git-rewriting-history.json";
+import gitBranches from "./git-branches.json";
+import gitRemoteRepositories from "./git-remote-repositories.json";
+import gitConfig from "./git-config.json";
+import gitLog from "./git-log.json";
+import gitDiff from "./git-diff.json";
+import gitReset from "./git-reset.json";
+import gitRebase from "./git-rebase.json";
+import gitPull from "./git-pull.json";
+import gitPush from "./git-push.json";
+import GitCommandsList from "./components/GitCommandsList";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { GitCommandsList },
+  data: () => {
+    return {
+      gitBasic,
+      gitUndoingChanges,
+      gitRewritingHistory,
+      gitBranches,
+      gitRemoteRepositories,
+      gitConfig,
+      gitLog,
+      gitDiff,
+      gitReset,
+      gitRebase,
+      gitPull,
+      gitPush
+    };
+  },
+};
 </script>
 
 <style>
@@ -21,7 +63,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/GitCommandsList.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <h2 class="commands-list-title">{{title}}</h2>
+  <table>
+    <tr>
+      <th>Commande</th>
+      <th>Description</th>
+    </tr>
+
+    <tr v-for="command in commands" v-bind:key="command.command">
+      <td>{{command.command}}</td>
+      <td>{{command.description}}</td>
+    </tr>
+  </table>
+</div>
+</template>
+
+<script>
+export default {
+  props: [
+    'title',
+    'commands',
+  ],
+};
+</script>
+
+<style lang="scss">
+    .commands-list-title {
+        text-transform: uppercase;
+    }
+</style>
