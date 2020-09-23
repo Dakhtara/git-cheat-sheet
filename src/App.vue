f2f007a9e0492095e1c573488a74cdd97d62feb7
Anthony Matignon <anthony.matignon@codes-rousseau.fr>
2020-09-23 09:22:59 +0200

    Change list of git commands

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,80 @@
 <template>
   <div id="app">
-    <h1>Git Cheat Sheet</h1>
+    <div class="page">
+      <h1>Git Cheat Sheet</h1>
 
-    <GitCommandsList title="Git Basic" :commands="gitBasic" />
+      <h2>Pratique de base</h2>
 
-    <GitCommandsList title="Annulation des moficiation" :commands="gitUndoingChanges" />
-    <GitCommandsList title="Réecrire l'historique" :commands="gitRewritingHistory" />
-    <GitCommandsList title="Git Branches" :commands="gitBranches" />
-    <GitCommandsList title="Dépôts Distants" :commands="gitRemoteRepositories" />
+      <p>
+        Sur un projet sous git, lorsque l'on travaille à plusieurs sur un projet, il est souvent nécessaire de
+        travailler chacun sur sa branche pendant
+        le développement et ensuite d'intégrer les modifications dans une branche commune.
+      </p>
 
-    <br />
-    <h1>Options additionnels</h1>
+      <p>
+        La branche
+        <b>master</b> représente la branche de production.
+        <br/>La branche
+        <b>develop</b> représente la branche de développement, c'est sur celle-ci que l'on doit intégrer nos
+        modifications.
+      </p>
 
-    <GitCommandsList title="Git Config" :commands="gitConfig" />
-    <GitCommandsList title="Git Log" :commands="gitLog" />
-    <GitCommandsList title="Git Diff" :commands="gitDiff" />
-    <GitCommandsList title="Git Reset" :commands="gitReset" />
-    <GitCommandsList title="Git Rebase" :commands="gitRebase" />
-    <GitCommandsList title="Git Pull" :commands="gitPull" />
-    <GitCommandsList title="Git Push" :commands="gitPush" />
+      <h3>Déroulement d'un dévelopement sous Git</h3>
+
+      <p>
+        Dans notre exemple, nous devons développer la possibilité de modifier les mots de passes des utilisateurs.
+        <br>On se met sur la branche
+        <b>develop</b>.
+        <br>On récupère éventuellement les modifications pour être à jour sur la branche (git fetch, git pull).
+        <br>On crée sa branche (git checkout -b features/password-request).
+        <br>
+        <br>On peut push la branche nouvellement créer avec 'git push --set-upstream &lt;remote&gt; features/password-request'.
+        <br>Une fois la branche créée, on peut ajouter nos premiers commit.
+        <br>
+        <br>Une fois la branche terminée, il suffit de créer une pull request sur l’interface web (Github, Gitlab etc.)
+        et faire la demande de merge dans la branche
+        <b>develop</b>.
+      </p>
+
+      <h3>Faire un hotfix</h3>
+      <p>Parfois il arrive que des problèmes remontent dans l'environnement de production. Une branche "hotfix" est
+        alors utilisé pour corriger ces problèmes.<br>
+        Cette branche part de 'master' et est fusionnée avec la branche de développement et la branche de
+        production.<br>
+      </p>
+      <code>
+        git flow hotfix start hotfix-name
+      </code>
+      <p>Une fois la branche crée, on effectue les modifications dans cette branche de hotfix. Pour terminer le hotfix
+        il suffit de faire</p>
+      <code>
+        git flow hotfix finish 'hotfix-name'
+      </code>
+    </div>
+    <div class="page">
+      <h1>Commande de bases</h1>
+      <div class="list-commands">
+        <GitCommandsList title="Git Basic" :commands="gitBasic"/>
+        <GitCommandsList title="Annulation des modificiations" :commands="gitUndoingChanges"/>
+        <GitCommandsList title="Réécrire l'historique" :commands="gitRewritingHistory"/>
+        <GitCommandsList title="Git Branches" :commands="gitBranches"/>
+        <GitCommandsList title="Dépôts Distants" :commands="gitRemoteRepositories"/>
+      </div>
+    </div>
+    <br/>
+    <div class="page">
+      <h1>Options additionnels</h1>
+
+      <div class="list-commands">
+        <GitCommandsList title="Git Config" :commands="gitConfig"/>
+        <GitCommandsList title="Git Log" :commands="gitLog"/>
+        <GitCommandsList title="Git Diff" :commands="gitDiff"/>
+        <GitCommandsList title="Git Reset" :commands="gitReset"/>
+        <GitCommandsList title="Git Rebase" :commands="gitRebase"/>
+        <GitCommandsList title="Git Pull" :commands="gitPull"/>
+        <GitCommandsList title="Git Push" :commands="gitPush"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,9 +92,10 @@ import gitRebase from "./git-rebase.json";
 import gitPull from "./git-pull.json";
 import gitPush from "./git-push.json";
 import GitCommandsList from "./components/GitCommandsList";
+
 export default {
   name: "App",
-  components: { GitCommandsList },
+  components: {GitCommandsList},
   data: () => {
     return {
       gitBasic,
@@ -52,18 +109,31 @@ export default {
       gitReset,
       gitRebase,
       gitPull,
-      gitPush
+      gitPush,
     };
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 1830px;
+  margin: auto;
+}
+
+.list-commands {
+  display: grid;
+  flex-wrap: wrap;
+  grid-gap: 10px;
+  grid-template-columns: 50% 50%;
+}
+
+.page {
+  break-after: always;
+  page-break-after: always;
 }
 </style>
--- src/components/GitCommandsList.vue
+++ src/components/GitCommandsList.vue
@@ -1,31 +1,50 @@
 <template>
-<div>
-  <h2 class="commands-list-title">{{title}}</h2>
-  <table>
-    <tr>
-      <th>Commande</th>
-      <th>Description</th>
-    </tr>
-
-    <tr v-for="command in commands" v-bind:key="command.command">
-      <td>{{command.command}}</td>
-      <td>{{command.description}}</td>
-    </tr>
-  </table>
-</div>
+  <div class="commands-list">
+    <h2 class="commands-list-title">{{title}}</h2>
+    <table class="commands-list-table">
+      <tr v-for="command in commands" v-bind:key="command.command">
+        <td class="right-border">{{command.command}}</td>
+        <td>{{command.description}}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-  props: [
-    'title',
-    'commands',
-  ],
+  props: ["title", "commands"],
 };
 </script>
 
 <style lang="scss">
-    .commands-list-title {
-        text-transform: uppercase;
+.commands-list {
+    max-width: 100%;
+    page-break-inside: avoid;
+    break-inside: avoid-page;
+      -webkit-column-break-inside: avoid;
+}
+.commands-list-title {
+  text-transform: uppercase;
+  color: #58ade3;
+  font-size: 16px;
+}
+
+.commands-list-table {
+    border-top: 2px solid #58ade3;
+    border-collapse: collapse;
+    width: 100%;
+    font-size: 12px;
+    break-inside: avoid-page;
+    & td, & th {
+        padding: 10px;
     }
+    tr:nth-child(even) {
+        background-color: #F2F2F2;
+    }
+}
+
+.right-border {
+    border-right: 1px solid #aeaeae;
+    width: 25%;
+}
 </style>
